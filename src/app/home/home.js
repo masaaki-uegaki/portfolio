b001cccaa222f1ee136e258c2092f59e7d2d1b66
上垣正顕 <uwettie@uegakimasaakinoiMac.local>
2018-03-02 05:39:08 +0900

    Catch copy

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -1,5 +1,17 @@
-module.exports = {
-  exec: () => {
-    console.log('home');
-  }
-};
+import * as $ from 'jquery';
+
+export function exec() {
+  console.log('home');
+  $('document').ready(() => {
+    let copy = ['Masaaki Uegaki,', 'System engineer,', 'Welcome to my porfolio!'],
+      i = 0;
+    setInterval(function catchcopy() {
+      let cc = $('.catchcopy');
+      if (cc) {
+        cc.text(copy[i++])
+        if (i > copy.length) i = 0;
+      };
+      return catchcopy;
+    }(), 3000);
+  })
+}
