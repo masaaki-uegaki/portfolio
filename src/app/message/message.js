bf33777c37625ca8c1c6b84f6ab9dd17eb5a621e
上垣正顕 <uwettie@uegakimasaakinoiMac.local>
2018-03-03 03:09:28 +0900

    Change button

--- src/app/message/message.js
+++ src/app/message/message.js
@@ -1,5 +1,11 @@
 module.exports = {
   exec: () => {
     console.log('message');
+    $(document).ready(() => {
+      $('#submit').click(() => {
+        // $('#message-form').submit();
+        alert('メッセージ送信機能は未実装です');
+      });
+    });
   }
 };
